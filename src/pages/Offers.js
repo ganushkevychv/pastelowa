8dab4ac00f55b9f48109e74a89f8321a51e6dace
ganushkevychv <ganushkevychv@gmail.com>
2022-10-06 14:41:18 +0300

    Offers page styles corrected

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -4,10 +4,10 @@ import Cards from '../components/Cards';
 const Offers = () => {
 
     return(
-        <>
+        <div className="Offers">
         <Menu/>
         <Cards/>
-        </>
+        </div>
     )
 }
 export default Offers;
