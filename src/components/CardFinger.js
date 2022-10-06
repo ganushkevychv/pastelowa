ada5c86aa2941eac1f674cecfda66c7097eeb0eb
ganushkevychv <ganushkevychv@gmail.com>
2022-10-06 13:44:42 +0300

    CardFinger component 5 new cards and logoColor added to card,background-color was changed to black

--- src/components/CardFinger.js
+++ src/components/CardFinger.js
@@ -9,14 +9,15 @@ import {
   MDBRow,
   MDBCol
 } from 'mdb-react-ui-kit';
+import logo from '../images/logoColor.png'
 
 const CardFinger = () => {
   return (
-    <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
+    <MDBRow className='row-cols-1 row-cols-md-4 g-7'>
       <MDBCol>
         <MDBCard className='h-100'>
           <MDBCardImage
-            src='https://mdbootstrap.com/img/new/standard/city/044.webp'
+            src={logo}
             alt='...'
             position='top'
           />
@@ -28,14 +29,14 @@ const CardFinger = () => {
             </MDBCardText>
           </MDBCardBody>
           <MDBCardFooter>
-            <small className='text-muted'>Last updated 3 mins ago</small>
+            <small>Last updated 3 mins ago</small>
           </MDBCardFooter>
         </MDBCard>
       </MDBCol>
       <MDBCol>
         <MDBCard className='h-100'>
           <MDBCardImage
-            src='https://mdbootstrap.com/img/new/standard/city/043.webp'
+            src={logo}
             alt='...'
             position='top'
           />
@@ -46,14 +47,14 @@ const CardFinger = () => {
             </MDBCardText>
           </MDBCardBody>
           <MDBCardFooter>
-            <small className='text-muted'>Last updated 3 mins ago</small>
+            <small>Last updated 3 mins ago</small>
           </MDBCardFooter>
         </MDBCard>
       </MDBCol>
       <MDBCol>
         <MDBCard className='h-100'>
           <MDBCardImage
-            src='https://mdbootstrap.com/img/new/standard/city/042.webp'
+            src={logo}
             alt='...'
             position='top'
           />
@@ -65,10 +66,107 @@ const CardFinger = () => {
             </MDBCardText>
           </MDBCardBody>
           <MDBCardFooter>
-            <small className='text-muted'>Last updated 3 mins ago</small>
+            <small>Last updated 3 mins ago</small>
           </MDBCardFooter>
         </MDBCard>
       </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src={logo}
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This
+              card has even longer content than the first to show that equal height action.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src={logo}
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This
+              card has even longer content than the first to show that equal height action.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src={logo}
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This
+              card has even longer content than the first to show that equal height action.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src={logo}
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This
+              card has even longer content than the first to show that equal height action.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol>
+        <MDBCard className='h-100'>
+          <MDBCardImage
+            src={logo}
+            alt='...'
+            position='top'
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This
+              card has even longer content than the first to show that equal height action.
+            </MDBCardText>
+          </MDBCardBody>
+          <MDBCardFooter>
+            <small>Last updated 3 mins ago</small>
+          </MDBCardFooter>
+        </MDBCard>
+      </MDBCol>
+      
+      
     </MDBRow>
   );
 }
--- src/pages/FingerFood.js
+++ src/pages/FingerFood.js
@@ -1,13 +1,15 @@
-import Menu from "../components/Menu";
+import Menu from "../components/Menu"
+import CardFinger from "../components/CardFinger";
 
 
 const FingerFood = () => {
 
     return (
-        <>
+        <div className="FingerFood">
         <Menu/>
         <h1>FingerFood</h1>
-        </>
+        <CardFinger/>
+        </div>
     )
 }
 export default FingerFood;
